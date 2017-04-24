f75744e7cb981a316cb5ae90f8e9028527a20a8b
TobyOoO <tobbymailbox@gmail.com>
2017-04-24 21:50:06 +0800

    init.

---
+++ ic-gist/js/script.js
@@ -0,0 +1,19 @@
+
+
+/*function scrollAni(event){
+	if(event){
+		event.preventDefault();
+	}
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500, function () {
+        window.location.hash = href;
+    });
+    return false;
+}
+
+$('a[href*=#]').on('click', scrollAni);
+
+$(function(){
+	if (window.location.hash.indexOf('#')>=0){
+		scrollAni();
+	}
+});*/
